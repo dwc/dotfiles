1250a9df9abaa0fc6b2d1690b32b73e3e3634eef
Daniel Westermann-Clark <github@danieltwc.me>
2023-06-26 21:50:09 -0700

    Add esp-idf development alias

--- .zshrc.d/030-aliases.sh
+++ .zshrc.d/030-aliases.sh
@@ -5,3 +5,6 @@ alias screen="screen -c $HOME/.screenrc -R -d"
 alias less="less -R"
 
 alias ec="emacsclient"
+
+# https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html#step-4-set-up-the-environment-variables
+alias get_idf=". $HOME/src/esp/esp-idf/export.sh"
