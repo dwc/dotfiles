3b6093ea1746188951fa1d8a8315cfc6d799307f
Daniel Westermann-Clark <github@danieltwc.me>
2024-07-13 12:39:16 -0700

    Start to script macOS setup

---
+++ bin/setup-mac.sh
@@ -0,0 +1,23 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install aspell && \
+    brew install awscli && \
+    brew install bartender && \
+    brew install dropbox && \
+    brew install firefox && \
+    brew install font-ibm-plex && \
+    brew install google-chrome && \
+    brew install istat-menus && \
+    brew install iterm2 && \
+    brew install jq && \
+    brew install keepassxc && \
+    brew install libreoffice && \
+    brew install md5sha1sum && \
+    brew install scroll-reverser && \
+    brew install signal && \
+    brew install slack && \
+    brew install tree && \
+    brew install visual-studio-code && \
+    brew install watch && \
+    brew install xquartz && \
+    brew install zoom
